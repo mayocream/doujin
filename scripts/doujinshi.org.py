17665d04bbff1e6aebd5ff2291dc09acceab3177
Mayo <mayo@linux.com>
2025-04-05 20:40:04 +0900

    Refactor processing functions to improve concurrency: separate circle and author processing, enhance progress bar handling, and streamline DataFrame creation.

--- scripts/doujinshi.org.py
+++ scripts/doujinshi.org.py
@@ -6,20 +6,49 @@ import polars as pl
 import glob
 import orjson
 from tqdm import tqdm
+import concurrent.futures
 
 
 def read_json(file_path):
     """Read a JSON file and return a DataFrame."""
     with open(file_path, "rb") as f:
         data = orjson.loads(f.read())
-    df = pl.json_normalize(data, max_level=0, strict=False)
+    df = pl.json_normalize(data, strict=False)
     return df
 
 
-def process_authors():
+def process_circles(position=0):
     """Process all JSON files in a directory."""
     dfs = []
-    for file_path in tqdm(glob.glob("data/doujinshi.org/Author/*.json")):
+    for file_path in tqdm(
+        glob.glob("data/doujinshi.org/Circle/*.json"),
+        desc="Processing Circles",
+        position=position,
+    ):
+        df = read_json(file_path)
+        df = df.select(
+            [pl.col("@ID"), pl.col("NAME_JP"), pl.col("NAME_EN"), pl.col("NAME_R")]
+        )
+        dfs.append(df)
+
+    # Concatenate all DataFrames into one
+    df = pl.concat(dfs, how="diagonal_relaxed")
+    df = df.rename(
+        {"@ID": "id", "NAME_JP": "name", "NAME_EN": "name_en", "NAME_R": "name_romaji"}
+    )
+    df = df.with_columns(pl.col("id").str.replace("C", ""))
+
+    df.write_csv("data/doujinshi.org/circles.csv")
+
+
+def process_authors(position=0):
+    """Process all JSON files in a directory."""
+    dfs = []
+    for file_path in tqdm(
+        glob.glob("data/doujinshi.org/Author/*.json"),
+        desc="Processing Authors",
+        position=position,
+    ):
         df = read_json(file_path)
         df = df.select(
             [pl.col("@ID"), pl.col("NAME_JP"), pl.col("NAME_EN"), pl.col("NAME_R")]
@@ -37,4 +66,10 @@ def process_authors():
 
 
 if __name__ == "__main__":
-    process_authors()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        concurrent.futures.wait(
+            [
+                executor.submit(process_authors, 0),
+                executor.submit(process_circles, 1),
+            ]
+        )
