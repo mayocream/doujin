51e27663f5b7d0b025e000703c4503245e9f001b
Mayo <mayo@linux.com>
2025-04-05 21:08:23 +0900

    Enhance data processing functions: implement content and character processing, streamline DataFrame selection, and improve concurrency with executor submissions.

--- scripts/doujinshi.org.py
+++ scripts/doujinshi.org.py
@@ -17,6 +17,84 @@ def read_json(file_path):
     return df
 
 
+def process_books(position=0):
+    # TODO
+    return None
+
+
+def process_contents(position=0):
+    """Process all JSON files in a directory."""
+    dfs = []
+    for file_path in tqdm(
+        glob.glob("data/doujinshi.org/Content/*.json"),
+        desc="Processing Contents",
+        position=position,
+    ):
+        df = read_json(file_path)
+        df = df.select(
+            [
+                pl.col("@ID"),
+                pl.col("NAME_JP"),
+                pl.col("NAME_EN"),
+                pl.col("NAME_R"),
+            ]
+        )
+        dfs.append(df)
+
+    # Concatenate all DataFrames into one
+    df = pl.concat(dfs, how="diagonal_relaxed")
+    df = df.rename(
+        {
+            "@ID": "id",
+            "NAME_JP": "name",
+            "NAME_EN": "name_en",
+            "NAME_R": "name_romaji",
+        }
+    )
+    df = df.with_columns(pl.col("id").str.replace("K", ""))
+
+    df.write_csv("data/doujinshi.org/contents.csv")
+
+
+def process_characters(position=0):
+    """Process all JSON files in a directory."""
+    dfs = []
+    for file_path in tqdm(
+        glob.glob("data/doujinshi.org/Character/*.json"),
+        desc="Processing Characters",
+        position=position,
+    ):
+        df = read_json(file_path)
+        df = df.select(
+            [
+                pl.col("@ID"),
+                pl.col("NAME_JP"),
+                pl.col("NAME_EN"),
+                pl.col("NAME_R"),
+                pl.col("DATA_SEX"),
+                pl.col("DATA_AGE"),
+            ]
+        )
+        dfs.append(df)
+
+    # Concatenate all DataFrames into one
+    df = pl.concat(dfs, how="diagonal_relaxed")
+    df = df.rename(
+        {
+            "@ID": "id",
+            "NAME_JP": "name",
+            "NAME_EN": "name_en",
+            "NAME_R": "name_romaji",
+            "DATA_SEX": "sex",
+            "DATA_AGE": "age",
+        }
+    )
+    df = df.with_columns(pl.col("id").str.replace("H", ""))
+
+    df.write_csv("data/doujinshi.org/characters.csv")
+
+
+# We ignore the relationships of circles and authors for lack of data
 def process_circles(position=0):
     """Process all JSON files in a directory."""
     dfs = []
@@ -71,5 +149,7 @@ if __name__ == "__main__":
             [
                 executor.submit(process_authors, 0),
                 executor.submit(process_circles, 1),
+                executor.submit(process_characters, 2),
+                executor.submit(process_contents, 3),
             ]
         )
