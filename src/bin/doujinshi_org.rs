927cdfc0c605d5b63c9555dcc10ff0445951e8a0
Mayo <mayo@linux.com>
2025-04-06 00:13:43 +0900

    Enhance concurrency in data processing: integrate Rayon for parallel processing of JSON files in books, contents, and authors, and update dependencies in Cargo.toml and Cargo.lock.

--- src/bin/doujinshi_org.rs
+++ src/bin/doujinshi_org.rs
@@ -1,86 +1,121 @@
+use std::sync::{Arc, Mutex};
+
+use futures::future;
 use glob::glob;
-use indicatif::ProgressIterator;
+use indicatif::ParallelProgressIterator;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
-async fn process_books() -> Result<(), Box<dyn std::error::Error>> {
-    let mut writer = csv::Writer::from_path("data/doujinshi.org/books.csv")?;
-    writer.write_record(&["id", "name", "name_en", "name_romaji", "name_alt"])?;
-    for path in glob("data/doujinshi.org/Book/*.json")?
+async fn process_books() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let writer = Arc::new(Mutex::new(csv::Writer::from_path(
+        "data/doujinshi.org/books.csv",
+    )?));
+    writer
+        .lock()
+        .unwrap()
+        .write_record(&["id", "name", "name_en", "name_romaji", "name_alt"])?;
+
+    glob("data/doujinshi.org/Book/*.json")?
         .filter_map(|entry| entry.ok())
         .collect::<Vec<_>>()
-        .iter()
+        .par_iter()
         .progress()
-    {
-        let file = std::fs::File::open(&path)?;
-        let reader = std::io::BufReader::new(file);
-        let data: serde_json::Value = serde_json::from_reader(reader)?;
-        writer.write_record(&[
-            data["@ID"].as_str().unwrap().replace("B", ""),
-            data["NAME_JP"].as_str().unwrap_or_default().to_string(),
-            data["NAME_EN"].as_str().unwrap_or_default().to_string(),
-            data["NAME_R"].as_str().unwrap_or_default().to_string(),
-            data["NAME_ALT"].as_str().unwrap_or_default().to_string(),
-        ])?;
-    }
+        .for_each(|path| {
+            if let Ok(file) = std::fs::File::open(path) {
+                let reader = std::io::BufReader::new(file);
+                if let Ok(data) = serde_json::from_reader::<_, serde_json::Value>(reader) {
+                    let mut writer = writer.lock().unwrap();
+                    let _ = writer.write_record(&[
+                        data["@ID"].as_str().unwrap_or_default().replace("B", ""),
+                        data["NAME_JP"].as_str().unwrap_or_default().to_string(),
+                        data["NAME_EN"].as_str().unwrap_or_default().to_string(),
+                        data["NAME_R"].as_str().unwrap_or_default().to_string(),
+                        data["NAME_ALT"].as_str().unwrap_or_default().to_string(),
+                    ]);
+                }
+            }
+        });
 
-    writer.flush()?;
+    writer.lock().unwrap().flush()?;
     Ok(())
 }
 
-async fn process_contents() -> Result<(), Box<dyn std::error::Error>> {
-    let mut writer = csv::Writer::from_path("data/doujinshi.org/contents.csv")?;
-    writer.write_record(&["id", "name", "name_en", "name_romaji", "name_alt"])?;
-    for path in glob("data/doujinshi.org/Content/*.json")?
+async fn process_contents() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let writer = Arc::new(Mutex::new(csv::Writer::from_path(
+        "data/doujinshi.org/contents.csv",
+    )?));
+    writer
+        .lock()
+        .unwrap()
+        .write_record(&["id", "name", "name_en", "name_romaji", "name_alt"])?;
+
+    glob("data/doujinshi.org/Content/*.json")?
         .filter_map(|entry| entry.ok())
         .collect::<Vec<_>>()
-        .iter()
+        .par_iter()
         .progress()
-    {
-        let file = std::fs::File::open(&path)?;
-        let reader = std::io::BufReader::new(file);
-        let data: serde_json::Value = serde_json::from_reader(reader)?;
-        writer.write_record(&[
-            data["@ID"].as_str().unwrap().replace("C", ""),
-            data["NAME_JP"].as_str().unwrap_or_default().to_string(),
-            data["NAME_EN"].as_str().unwrap_or_default().to_string(),
-            data["NAME_R"].as_str().unwrap_or_default().to_string(),
-            data["NAME_ALT"].as_str().unwrap_or_default().to_string(),
-        ])?;
-    }
+        .for_each(|path| {
+            if let Ok(file) = std::fs::File::open(path) {
+                let reader = std::io::BufReader::new(file);
+                if let Ok(data) = serde_json::from_reader::<_, serde_json::Value>(reader) {
+                    let mut writer = writer.lock().unwrap();
+                    let _ = writer.write_record(&[
+                        data["@ID"].as_str().unwrap_or_default().replace("K", ""),
+                        data["NAME_JP"].as_str().unwrap_or_default().to_string(),
+                        data["NAME_EN"].as_str().unwrap_or_default().to_string(),
+                        data["NAME_R"].as_str().unwrap_or_default().to_string(),
+                        data["NAME_ALT"].as_str().unwrap_or_default().to_string(),
+                    ]);
+                }
+            }
+        });
 
-    writer.flush()?;
+    writer.lock().unwrap().flush()?;
     Ok(())
 }
 
-async fn process_authors() -> Result<(), Box<dyn std::error::Error>> {
-    let mut writer = csv::Writer::from_path("data/doujinshi.org/authors.csv")?;
-    writer.write_record(&["id", "name", "name_en", "name_romaji", "name_alt"])?;
-    for path in glob("data/doujinshi.org/Author/*.json")?
+async fn process_authors() -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+    let writer = Arc::new(Mutex::new(csv::Writer::from_path(
+        "data/doujinshi.org/authors.csv",
+    )?));
+    writer
+        .lock()
+        .unwrap()
+        .write_record(&["id", "name", "name_en", "name_romaji", "name_alt"])?;
+
+    glob("data/doujinshi.org/Author/*.json")?
         .filter_map(|entry| entry.ok())
         .collect::<Vec<_>>()
-        .iter()
+        .par_iter()
         .progress()
-    {
-        let file = std::fs::File::open(&path)?;
-        let reader = std::io::BufReader::new(file);
-        let data: serde_json::Value = serde_json::from_reader(reader)?;
-        writer.write_record(&[
-            data["@ID"].as_str().unwrap().replace("A", ""),
-            data["NAME_JP"].as_str().unwrap_or_default().to_string(),
-            data["NAME_EN"].as_str().unwrap_or_default().to_string(),
-            data["NAME_R"].as_str().unwrap_or_default().to_string(),
-            data["NAME_ALT"].as_str().unwrap_or_default().to_string(),
-        ])?;
-    }
+        .for_each(|path| {
+            if let Ok(file) = std::fs::File::open(path) {
+                let reader = std::io::BufReader::new(file);
+                if let Ok(data) = serde_json::from_reader::<_, serde_json::Value>(reader) {
+                    let mut writer = writer.lock().unwrap();
+                    let _ = writer.write_record(&[
+                        data["@ID"].as_str().unwrap_or_default().replace("A", ""),
+                        data["NAME_JP"].as_str().unwrap_or_default().to_string(),
+                        data["NAME_EN"].as_str().unwrap_or_default().to_string(),
+                        data["NAME_R"].as_str().unwrap_or_default().to_string(),
+                        data["NAME_ALT"].as_str().unwrap_or_default().to_string(),
+                    ]);
+                }
+            }
+        });
 
-    writer.flush()?;
+    writer.lock().unwrap().flush()?;
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    process_authors().await?;
-    process_contents().await?;
-    process_books().await?;
+    let tasks = vec![
+        tokio::spawn(process_authors()),
+        tokio::spawn(process_contents()),
+        tokio::spawn(process_books()),
+    ];
+
+    future::join_all(tasks).await;
 
     Ok(())
 }
