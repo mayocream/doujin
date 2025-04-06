e4d1a8f3aa9322d154a42ba3a9456e01b40d69f7
Mayo <mayo@linux.com>
2025-04-06 19:18:37 +0900

    Enhance character processing: add async functions for processing characters and character tags, improving data extraction and organization with detailed progress indicators.

--- src/bin/doujinshi_org.rs
+++ src/bin/doujinshi_org.rs
@@ -3,10 +3,16 @@ use std::{path::PathBuf, sync::LazyLock};
 use anyhow::Result;
 use futures::future;
 use glob::glob;
-use indicatif::{ParallelProgressIterator, ProgressIterator};
+use indicatif::{MultiProgress, ParallelProgressIterator, ProgressBar, ProgressIterator};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 static DATA_DIR: LazyLock<PathBuf> = LazyLock::new(|| PathBuf::from("data/doujinshi.org"));
+static MULTT_PROGRESS: LazyLock<MultiProgress> = LazyLock::new(MultiProgress::new);
+
+fn progress_bar() -> ProgressBar {
+    MULTT_PROGRESS
+        .add(ProgressBar::new_spinner().with_style(indicatif::ProgressStyle::default_spinner()))
+}
 
 async fn process<F>(pattern: &str, output_file: &str, header: Vec<&str>, transform: F) -> Result<()>
 where
@@ -19,7 +25,9 @@ where
         .filter_map(Result::ok)
         .collect::<Vec<_>>()
         .par_iter()
-        .progress()
+        .progress_with(
+            progress_bar().with_message(format!("Processing files matching pattern: {}", pattern)),
+        )
         .map(|file| {
             let file = std::fs::File::open(file).unwrap();
             let reader = std::io::BufReader::new(file);
@@ -28,7 +36,7 @@ where
         })
         .collect::<Vec<_>>()
         .into_iter()
-        .progress()
+        .progress_with(progress_bar().with_message("Writing records"))
         .for_each(|record| {
             writer.write_record(&record).unwrap();
         });
@@ -61,9 +69,63 @@ async fn process_authors() {
     .unwrap()
 }
 
+async fn process_characters() {
+    process(
+        &DATA_DIR.join("Character").join("*.json").to_string_lossy(),
+        &DATA_DIR.join("characters.csv").to_string_lossy(),
+        vec!["id", "name", "name_en", "name_romaji", "name_alt"],
+        |json| {
+            vec![
+                json["@ID"].as_str().unwrap_or_default().replace("H", ""),
+                json["NAME_JP"].as_str().unwrap_or_default().to_string(),
+                json["NAME_EN"].as_str().unwrap_or_default().to_string(),
+                json["NAME_R"].as_str().unwrap_or_default().to_string(),
+                json["NAME_ALT"]
+                    .as_array()
+                    .map(|arr| arr.iter().filter_map(|v| v.as_str()).collect::<Vec<_>>())
+                    .unwrap_or_else(|| vec![json["NAME_ALT"].as_str().unwrap_or_default()])
+                    .join(","),
+            ]
+        },
+    )
+    .await
+    .unwrap()
+}
+
+async fn process_character_tags() {
+    process(
+        &DATA_DIR.join("Character").join("*.json").to_string_lossy(),
+        &DATA_DIR.join("character_tags.csv").to_string_lossy(),
+        vec!["character_id", "tag_id"],
+        |json| {
+            vec![
+                json["@ID"].as_str().unwrap_or_default().replace("H", ""),
+                json["LINKS"]["ITEM"]
+                    .as_array()
+                    .map(|arr| {
+                        arr.iter()
+                            .filter_map(|v| v.get("@ID").unwrap().as_str())
+                            .collect::<Vec<_>>()
+                    })
+                    .unwrap_or_else(|| {
+                        vec![json["LINKS"]["ITEM"]["@ID"].as_str().unwrap_or_default()]
+                    })
+                    .join(","),
+            ]
+        },
+    )
+    .await
+    .unwrap()
+}
+
 #[tokio::main]
 async fn main() {
-    future::join_all(vec![tokio::spawn(process_authors())]).await;
+    future::join_all(vec![
+        tokio::spawn(process_authors()),
+        tokio::spawn(process_characters()),
+        tokio::spawn(process_character_tags()),
+    ])
+    .await;
 
     println!("Processing completed.")
 }
